785720e57075cb6a49f999df9e65f8647438370b
ikudria <irakudrya@gmail.com>
2022-06-30 17:09:26 -0500

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,19 +9,6 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
-
-
-/* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div> */
-
 const galleryContainer = document.querySelector(".gallery");
 const cardsMarkup = createGalleryMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
