f3137586a8938972a81cc612570ca9277da2af64
ikudria <irakudrya@gmail.com>
2022-07-02 14:09:46 -0500

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from 'lodash.throttle';
 
+const FEEDBACK_FORM = "feedback-form-state";
+const form = document.querySelector('.feedback-form');
+// console.log(form);
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+const dataForm = {};
+
+function onFormInput(event) {
+
+    const { target } = event;
+    const elTargetName = target.name;
+    const elTargetValue = target.value;
+    // console.log(target);
+
+    
+    dataForm[elTargetName] = elTargetValue;
+
+    // console.log(dataForm);
+
+    localStorage.setItem(FEEDBACK_FORM, JSON.stringify(dataForm));
+
+}
+
+function populateForm () {
+    const formData = JSON.parse(localStorage.getItem(FEEDBACK_FORM));
+    // console.log(formData);
+
+    for (const key in formData) {
+        const inputEl = form.elements[key];
+        const inputValue = formData[key];
+        // console.log(inputEl);
+        // console.log(inputValue);
+
+        inputEl.value = inputValue;
+    }
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    
+    event.currentTarget.reset();
+    console.log(JSON.parse(localStorage.getItem(FEEDBACK_FORM)));
+    localStorage.removeItem(FEEDBACK_FORM);
+    
+}
+
+
+populateForm();
